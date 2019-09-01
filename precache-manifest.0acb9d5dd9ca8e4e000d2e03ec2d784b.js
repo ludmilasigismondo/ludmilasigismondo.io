eaf56c87e9b929bbea56f88e331f0601a079984e
finw3 <nestor.2005@gmail.com>
2019-09-01 18:34:52 -0300

    Updates

---
+++ precache-manifest.0acb9d5dd9ca8e4e000d2e03ec2d784b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860ec10458e65aefa6aa43326d6d1631",
+    "url": "/ludmilasigismondo.github.io/index.html"
+  },
+  {
+    "revision": "c1669a8a0622d64b23d1",
+    "url": "/ludmilasigismondo.github.io/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "67db125b141a46489ac4",
+    "url": "/ludmilasigismondo.github.io/static/css/main.cc381547.chunk.css"
+  },
+  {
+    "revision": "c1669a8a0622d64b23d1",
+    "url": "/ludmilasigismondo.github.io/static/js/2.467cf8e7.chunk.js"
+  },
+  {
+    "revision": "67db125b141a46489ac4",
+    "url": "/ludmilasigismondo.github.io/static/js/main.6c41059f.chunk.js"
+  },
+  {
+    "revision": "979b39a87cd44ef8e5e8",
+    "url": "/ludmilasigismondo.github.io/static/js/runtime~main.b115703e.js"
+  }
+]);
